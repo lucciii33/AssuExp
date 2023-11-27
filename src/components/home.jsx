9485110c4975bb4f7e468d72d82c2fb1868b1aac
Angelo Maiele <angelomaiele@gamil.com>
2023-11-27 16:50:39 -0500

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import '../styles/home.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div className='containerImage'>
+            <h1 className='test'>TEST</h1>
+
+    
+        </div>
+        
+    </>
+  )
+}
+
+export default Home
