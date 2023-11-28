bbd11851b5c3991e3856a10c55dcb8697967eb9f
Angelo Maiele <angelomaiele@gamil.com>
2023-11-27 19:09:42 -0500

    more work

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,9 +8,26 @@ function Home() {
   return (
     <>
         <div className='containerImage'>
-            <h1 className='test'>TEST</h1>
-
-    
+        <div className='backgroundOverlay'></div>
+            <div className='infoImage'>
+            <div className='grid justify-items-center md:justify-items-end mt-50'>
+                <p className='para-text font-thin '>Como el éndulo que nunca se detiene,<br/> seguimos en constante expansión...</p>
+            </div>
+                <div className='grid justify-items-center md:justify-items-end items-center mt-60'>
+                {/* <div className=''> */}
+                    <div className='mt-5'>
+                        <h1 className='font-title font-extralight'>BIENVENIDOS A<br/><strong className='font-extrabold'>ASSU EN EXPACION</strong></h1>
+                        <p className='text-white font-normal'>Tejiendo un mundo de posibilidades y expansión.</p>
+                    </div>
+                {/* </div> */}
+                <div className='mt-5 grid justify-items-start items-center buttons-rigth'>
+                    <div>
+                    <button className='me-4 button-grey'>TERAPIAS</button>
+                        <button className=' button-blue'>RESERVA ONLINE</button>
+                    </div>
+                    </div> 
+                </div>
+            </div>
         </div>
         
     </>
