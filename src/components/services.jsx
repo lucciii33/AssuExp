0742036fff8deb853df2c337e7da01d6f41a35ad
Angelo Maiele <angelomaiele@gamil.com>
2023-11-30 13:17:53 -0500

    carrousel

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,34 +3,17 @@ import { useState } from "react";
 import "../styles/services.css";
 
 function Services() {
+
   const cardData = [
-    { title: "Card 1", content: "This is the content for Card 1" },
-    { title: "Card 2", content: "This is the content for Card 2" },
-    { title: "Card 3", content: "This is the content for Card 3" },
-    { title: "Card 4", content: "This is the content for Card 4" },
-    { title: "Card 5", content: "This is the content for Card 5" },
-    // Add more cards as needed
+    { id: 1, text: 'Card 1 Content' },
+    { id: 2, text: 'Card 2 Content' },
+    { id: 3, text: 'Card 3 Content' },
+    { id: 4, text: 'Card 4 Content' },
+    { id: 5, text: 'Card 5 Content' },
+    { id: 6, text: 'Card 6 Content' },
   ];
 
-  const [currentCard, setCurrentCard] = useState(0);
-
-  const nextCard = () => {
-    setCurrentCard((prevCard) => (prevCard + 1) % cardData.length);
-  };
-
-  const prevCard = () => {
-    setCurrentCard(
-      (prevCard) => (prevCard - 1 + cardData.length) % cardData.length
-    );
-  };
-
-  const calculateOffset = (index) => {
-    return `${20 * (index - currentCard)}%`;
-  };
-//   const carouselStyle = {
-//     transform: `translateX(-${currentIndex * (100 / carouselData.length)}%)`,
-//     transition: "transform 0.5s ease-in-out",
-//   };
+ const duplicated = [...cardData, ...cardData, ...cardData]
 
   return (
     <>
@@ -40,23 +23,19 @@ function Services() {
             <h1 className="titleServices font-bold">MIS TERAPIAS</h1>
           </div>
         </div>
-<div className="flex items-center justify-center mt-20">
-<div className="carousel-container">
-      <button onClick={prevCard} className="prew">Preview</button>
-      <div className="cards-container">
-        {cardData.map((card, index) => (
-          <div key={index} className="card"style={{ transform: `translateX(${calculateOffset(index)})` }}>
-            <h2>{card.title}</h2>
-            <p>{card.content}</p>
-            {/* Add any other card content here */}
+
+
+        <div className="carousel-container pb-20 mt-10">
+      <div className="cards-wrapper">
+        {duplicated.map((card) => (
+          <div key={card.id} className="card">
+            {card.text}
           </div>
         ))}
       </div>
-      <button onClick={nextCard} className="prew">Next</button>
     </div>
 
-</div>
-  
+
       </div>
     </>
   );
