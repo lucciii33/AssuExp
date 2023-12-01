d8f3da5d6bd05a3d76ae9b17ed6a8d9c04f9aea7
Jose Velasquez <velasquezdenorajose@gmail.com>
2023-12-01 19:53:17 -0400

    :lipstick: Add Last banner responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from "./components/home"
+import Home from './components/home'
 import Services from './components/services'
 import QuienEs from './components/quienEs'
+import { LastBanner } from './components/last-banner'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <Home/>
-    <QuienEs/>
-    <Services/>
-   
-      
-    </>
-  )
+	return (
+		<>
+			<Home />
+			<QuienEs />
+			<Services />
+			<LastBanner />
+		</>
+	)
 }
 
 export default App
---
+++ src/components/last-banner.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+
+export const LastBanner = () => {
+	const [data, setData] = useState({
+		name: '',
+		email: '',
+		telephone: '',
+		message: '',
+	})
+
+	const handleChange = (e) => {
+		const { value, name } = e.target
+		setData({ ...data, [name]: value })
+	}
+
+	const handleSubmit = () => {
+		// Submit function
+	}
+
+	return (
+		<div className="containerImage xl:flex xl:flex-col-reverse ">
+			<div className="backgroundOverlay"></div>
+			<div className="min-w-[350px] max-w-[1620px] min-h-screen p-20 xl:p-0 xl:pl-28 flex flex-col sm:flex-col xl:flex-row-reverse gap-20 2xl:gap-[40%] ">
+				{/* Form */}
+				<form
+					className="flex flex-col justify-center items-center gap-5 w-full z-10"
+					onSubmit={handleSubmit}
+				>
+					<input
+						className="bg-[#D9D9D9A6] rounded-2xl w-64 xl:w-[80%] sm:w-96 py-2 px-4 placeholder:text-gray-800 outline-none"
+						type="text"
+						name="name"
+						placeholder="Nombre"
+						value={data.name}
+						onChange={handleChange}
+					/>
+					<input
+						type="text"
+						className="bg-[#D9D9D9A6] rounded-2xl w-64 xl:w-[80%] sm:w-96 py-2 px-4 placeholder:text-gray-800 outline-none"
+						name="email"
+						placeholder="Email"
+						value={data.email}
+						onChange={handleChange}
+					/>
+					<input
+						type="text"
+						className="bg-[#D9D9D9A6] rounded-2xl w-64 xl:w-[80%] sm:w-96 py-2 px-4 placeholder:text-gray-800 outline-none"
+						name="telephone"
+						placeholder="Telephone"
+						value={data.telephone}
+						onChange={handleChange}
+					/>
+					<textarea
+						type="text"
+						rows={4}
+						className="bg-[#D9D9D9A6] rounded-2xl w-64 xl:w-[80%] sm:w-96 py-2 px-4 placeholder:text-gray-800 outline-none"
+						name="message"
+						placeholder="Message"
+						value={data.message}
+						onChange={handleChange}
+					/>
+					<button
+						type="button"
+						className="rounded-2xl bg-[#133554] px-10 py-2 w-64 xl:w-[80%] sm:w-96 text-white"
+					>
+						CONTACTAME
+					</button>
+				</form>
+				{/* Cards container */}
+				<div className="w-full h-96 xl:h-screen flex justify-center xl:items-start xl:flex-col ">
+					<div className="bg-[#D9D9D97A] h-[35%] sm:h-[40%] xl:h-[50%] w-[80%] md:w-[60%] xl:w-[45%] rounded-2xl -rotate-6 absolute"></div>
+					<div className="bg-[#D9D9D97A] h-[35%] sm:h-[40%] xl:h-[50%] w-[80%] md:w-[60%] xl:w-[45%] rounded-2xl absolute text-black z-10">
+						<div className="flex flex-col justify-center items-center h-full ">
+							<div className="w-60 sm:w-96 text-center flex flex-col gap-1 sm:gap-5">
+								<h4 className="text-lg sm:text-3xl font-bold">
+									SUSCRIBETE A MIS PROGRAMAS SEMANALES
+								</h4>
+								<p className="text-xs sm:text-lg">
+									Assunta es una fuente de luz y sabiduría en el mundo de la
+									espiritualidad y el bienestar. Su dedicación a la armonía, la
+									expansión de la conciencia y la sanación ha dejado una huella
+									indeleble en la vida de quienes la conocen.
+								</p>
+								<button
+									type="button"
+									className="rounded-2xl bg-[#3C3D3E] px-10 py-2 text-white text-sm sm:text-lg"
+								>
+									SUSCRIBIRTE
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className="bg-[#D9D9D97A] h-[35%] sm:h-[40%] xl:h-[50%] w-[80%] md:w-[60%] xl:w-[45%] rounded-2xl rotate-6 absolute"></div>
+				</div>
+			</div>
+		</div>
+	)
+}
