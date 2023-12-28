9f23f8fa0f534f7db3eb352864e33eccefe9915f
Angelo Maiele <angelomaiele@gamil.com>
2023-12-27 22:15:01 -0500

    cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
-
+import PropTypes from "prop-types";
 import "../styles/podcast.css";
+import image from "../assets/programaTest.webp"
 
-function Card() {
+function Card({ image, backgroundColor, textColor, spanText, title, description }) {
   
-
   return (
     <>
       <div className="card-container">
+          <div>
+            <img src={image} className="imageaCard"/>
+          </div>
+          <div className="flex items-center justify-center flex-col">
+          <p className="text-center">{spanText}</p>
+        <h3 className="text-center">{title}</h3>
+        <p className="text-center">{description}</p>
+          </div>
+          <div className="lastheader" style={{backgroundColor: backgroundColor}}>
+            
+          </div>
        
       </div>
     </>
   );
 }
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  spanText: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
 
 export default Card;
--- src/components/podcast.jsx
+++ src/components/podcast.jsx
@@ -1,28 +1,81 @@
 import { useState } from "react";
 import "../styles/podcast.css";
 import Card from "./card";
+import image from "../assets/programaTest.webp"
+import image2 from "../assets/test2.webp"
+import image3 from "../assets/test3.webp"
+import image4 from "../assets/test4.webp"
+
 function Podcats() {
   
   return (
     <>
-    <div className="flex flex-col items-center mt-5">
+    <div className="flex flex-col items-center pt-10 bodyne">
       <div className="flex flex-col items-center gap-4 md:flex-row">
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
+       <Card  
+          image={image}
+          backgroundColor="#FF0067"
+          textColor="#ffffff"
+          spanText="LUGAR"
+          title="COMO MANTENER TU CASA"
+          description="Description 1"/>
+       <Card 
+        image={image2}
+        backgroundColor="#00B706"
+        textColor="#ffffff"
+        spanText="TRABAJO"
+        title="CAMINO A EMPRENDER"
+        description="Description 1"
+        />
+       <Card
+       image={image3}
+          backgroundColor="#009CA6"
+          textColor="#ffffff"
+          spanText="TRABAJO"
+          title="QUE TENER EN TU OFICINA"
+          description="Description 1"
+          />
+       <Card
+        image={image4}
+        backgroundColor="#FF3400"
+        textColor="#ffffff"
+        spanText="VIDA"
+        title="CARRO LIMPIO ENERGIA SIN CHOQUES"
+        description="Description 1"
+       />
       </div>
-      <div className="flex flex-col items-center gap-4 md:flex-row mt-4">
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
-      </div>
-      <div className="flex flex-col items-center gap-4 md:flex-row mt-3">
-       <Card/>
-       <Card/>
-       <Card/>
-       <Card/>
+      <div className="flex flex-col items-center gap-4 md:flex-row mt-10">
+      <Card  
+          image={image}
+          backgroundColor="#FF0067"
+          textColor="#ffffff"
+          spanText="LUGAR"
+          title="COMO MANTENER TU CASA"
+          description="Description 1"/>
+       <Card 
+        image={image2}
+        backgroundColor="#00B706"
+        textColor="#ffffff"
+        spanText="TRABAJO"
+        title="CAMINO A EMPRENDER"
+        description="Description 1"
+        />
+       <Card
+       image={image3}
+          backgroundColor="#009CA6"
+          textColor="#ffffff"
+          spanText="TRABAJO"
+          title="QUE TENER EN TU OFICINA"
+          description="Description 1"
+          />
+       <Card
+        image={image4}
+        backgroundColor="#FF3400"
+        textColor="#ffffff"
+        spanText="VIDA"
+        title="CARRO LIMPIO ENERGIA SIN CHOQUES"
+        description="Description 1"
+       />
       </div>
     </div>
     </>
